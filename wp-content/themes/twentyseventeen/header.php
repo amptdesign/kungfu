050774c4521a676f07393dd0031c745d59c55717
Ampt Design Admin <amptdesign@live.com>
2017-02-23 13:21:36 -0600

    Adjusted Homepage Style.

--- wp-content/themes/twentyseventeen/header.php
+++ wp-content/themes/twentyseventeen/header.php
@@ -18,14 +18,16 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
-
+<link href="https://fonts.googleapis.com/css?family=Lato:300" rel="stylesheet">
 <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
 						<div class="homepage-intro-header">Learn the Ancient Art of Shih Shui Kung Fu</div>
-						<div class="homepage-intro-text">A Scientific System of Excercise that Increases Sexual Funtion, Improves Overall Health and Promotes Longevity.</div>
+						<div class="homepage-intro-text-wrapper">
+							<div class="homepage-intro-text">A Scientific System of Excercise that Increases Sexual Funtion, Improves Overall Health and Promotes Longevity.</div>
+						</div> 
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentyseventeen' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
