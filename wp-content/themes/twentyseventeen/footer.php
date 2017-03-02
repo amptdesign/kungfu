fd6fda28c21450e4a47072f7ad044f906de8d652
Ampt Design Admin <amptdesign@live.com>
2017-03-01 18:45:28 -0600

    Ready to go live!

--- wp-content/themes/twentyseventeen/footer.php
+++ wp-content/themes/twentyseventeen/footer.php
@@ -18,25 +18,29 @@
 
 		<footer id="colophon" class="site-footer" role="contentinfo">
 			<div class="wrap">
-				<?php
-				get_template_part( 'template-parts/footer/footer', 'widgets' );
+				
+				<aside class="widget-area" role="complementary">
+					<div class="widget-column footer-widget-1">
+						<h2 class="widget-title">Find Us:</h2><div class="textwidget"><p><strong>Address</strong><br><a href="https://www.google.com/maps/place/405+Lake+Cook+Rd,+Deerfield,+IL+60015/@42.2062072,-87.8451517,15z/data=!4m5!3m4!1s0x880fc0b9281d293d:0xffd7237812597c67!8m2!3d42.1514422!4d-87.8379452">405 Lake Cook Road, Unit A3<br>Deerfield, IL 60015</a></p>
+					</div>
+									
+					</div>
+					<div class="widget-column footer-widget-2">
+						<h2 class="widget-title">&nbsp;</h2><div class="textwidget"><p><strong>Hours</strong><br>Monday—Friday: 9:00AM–5:00PM<br>Saturday &amp; Sunday: 11:00AM–3:00PM</p></div>		
+					</div>
+				</aside><!-- /end widget-area -->
 
-				if ( has_nav_menu( 'social' ) ) : 
-				//if (true == false) : ?>
-					<nav class="social-navigation" role="navigation" aria-label="<?php _e( 'Footer Social Links Menu', 'twentyseventeen' ); ?>">
-						<?php
-							wp_nav_menu( array(
-								'theme_location' => 'social',
-								'menu_class'     => 'social-links-menu',
-								'depth'          => 1,
-								'link_before'    => '<span class="screen-reader-text">',
-								'link_after'     => '</span>' . twentyseventeen_get_svg( array( 'icon' => 'chain' ) ),
-							) );
-						?>
-					</nav><!-- .social-navigation -->
-				<?php endif;?>
+				<h2 class="widget-title">STAY IN TOUCH:</h2>
 
-				<div class="footer-text-wrapper"><div class="copyright"> Ampt Design &copy; <a href="#" class="tooltip" title="Ahead of the Curve!">2010 - 2017</a> - Expanding your mind through superior Design! </div></div>
+				<nav class="social-navigation" role="navigation" aria-label="Footer Social Links Menu">
+				<div class="menu-social-links-menu-container"><ul id="menu-social-links-menu" class="social-links-menu"><li id="menu-item-31" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-31"><a href="https://www.yelp.com"><span class="screen-reader-text">Yelp</span><svg class="icon icon-yelp" aria-hidden="true" role="img"> <use href="#icon-yelp" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-yelp"></use> </svg></a></li>
+				<li id="menu-item-32" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-32"><a href="https://www.facebook.com/shihshuikungfu"><span class="screen-reader-text">Facebook</span><svg class="icon icon-facebook" aria-hidden="true" role="img"> <use href="#icon-facebook" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-facebook"></use> </svg></a></li>
+				<li id="menu-item-33" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-33"><a href="https://twitter.com/shihshuikungfu"><span class="screen-reader-text">Twitter</span><svg class="icon icon-twitter" aria-hidden="true" role="img"> <use href="#icon-twitter" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-twitter"></use> </svg></a></li>
+				<li id="menu-item-34" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-34"><a href="https://www.instagram.com/shihshuikungfu"><span class="screen-reader-text">Instagram</span><svg class="icon icon-instagram" aria-hidden="true" role="img"> <use href="#icon-instagram" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-instagram"></use> </svg></a></li>
+				<li id="menu-item-35" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-35"><a href="mailto:shihshuikungfu@example.com"><span class="screen-reader-text">Email</span><svg class="icon icon-envelope-o" aria-hidden="true" role="img"> <use href="#icon-envelope-o" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-envelope-o"></use> </svg></a></li>
+				</ul></div></nav><!-- .social-navigation -->
+
+				<div class="footer-text-wrapper">DESIGNED BY <a href="http://amptdesign.com">AMPT DESIGN</a></div>
 				
 			</div><!-- .wrap -->
 		</footer><!-- #colophon -->
